810e84b21dde20df00bdd5ab8b13181f3f446700
Various <apple123@mail.ustc.edu.cn>
2015-03-31 17:46:09 +0800

    update

---
+++ GroupListHeaderTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  GroupListHeaderTableViewCell.h
+//  LuoMiPhone
+//
+//  Created by Tim Geng on 3/31/15.
+//  Copyright (c) 2015 GF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroupListHeaderTableViewCell : UITableViewCell
+
+@end
---
+++ GroupListHeaderTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  GroupListHeaderTableViewCell.m
+//  LuoMiPhone
+//
+//  Created by Tim Geng on 3/31/15.
+//  Copyright (c) 2015 GF. All rights reserved.
+//
+
+#import "GroupListHeaderTableViewCell.h"
+
+@implementation GroupListHeaderTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
